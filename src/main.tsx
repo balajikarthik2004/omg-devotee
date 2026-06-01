ada77354deb5a4d475f0afb28d8c3d7fc940b096
Balajikrishnan2004 <balaji.krishnan@gwcdata.ai>
2026-06-01 21:49:55 +0530

    fixed

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
-import './i18n/config';
+import i18n from './i18n/config';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from '@tanstack/react-router';
 import { getRouter } from './router';
+import { I18nextProvider } from 'react-i18next';
 
 const router = getRouter();
 
@@ -14,6 +15,8 @@ declare module '@tanstack/react-router' {
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <I18nextProvider i18n={i18n}>
+      <RouterProvider router={router} />
+    </I18nextProvider>
   </React.StrictMode>
 );
